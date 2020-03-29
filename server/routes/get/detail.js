540158e694dd9699419447a7f74e72e22ca2a5a4
Chelsea <chelsea.doeleman@gmail.com>
2020-03-29 16:09:45 +0200

    feat(detail.js): add check removeButton

--- server/routes/get/detail.js
+++ server/routes/get/detail.js
@@ -3,17 +3,20 @@ import User from '../../database/models/user.js'
 
 export const detail = async (req, res) => {
   const { username } = req.params
-  console.log(username)
   if (req.user) {
     const avatar = '/assets/images/avatar.svg'
     await User.findOne({ username: username }, async (err, results) => {
       if (err) return res.redirect('back')
 
       if (results) {
-        console.log(results)
-
+        const matches = await User.find({ _id: { $in: req.user.matched } }, results => results)
+        const matchName = matches.map(match => {
+          const name = match.username
+          return name
+        })
         res.render('detail', {
           data: results,
+          removeButton: matchName[0] === username || !matchName === undefined ? true : false,
           navigation: dataNavigation,
           username: req.user.username,
           avatar: req.user.avatar || avatar,
