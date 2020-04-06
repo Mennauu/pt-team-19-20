e4a8f4bdd2e13098a88f1fd233898b5dc12212ff
Robin Stut <robin@voorhoede.nl>
2020-04-06 14:37:56 +0200

    feat(events.js): adding cron job, adding try catch block

--- server/middleware/cron/events.js
+++ server/middleware/cron/events.js
@@ -1,7 +1,13 @@
+import cron from 'cron'
 import puppeteer from 'puppeteer'
 
-// What is a better file/directory to place this in?
-export default async function getFestivalData() {
+// Job starts when clock hits the 0 hour, the 0 minute
+const job = cron.job('0 0 * * *', () => {
+  getFestivalData()
+})
+export default job.start()
+
+async function getFestivalData() {
   // Maybe we need to retreive this data from the database in the future...
   const musicGenres = [
     'deephouse',
@@ -17,56 +23,60 @@ export default async function getFestivalData() {
     'urban',
   ]
 
-  puppeteer.launch().then(async browser => {
-    const page = await browser.newPage()
-    let mergedData = []
+  try {
+    puppeteer.launch().then(async browser => {
+      const page = await browser.newPage()
+      let mergedData = []
 
-    for (const [i, genre] of musicGenres.entries()) {
-      await page.goto(`https://festivalfans.nl/events/${genre}/`)
+      for (const [i, genre] of musicGenres.entries()) {
+        await page.goto(`https://festivalfans.nl/events/${genre}/`)
 
-      const data = await page.evaluate(() =>
-        [...document.querySelectorAll('div.ev2page script')].map(elem => elem.innerText),
-      )
+        const data = await page.evaluate(() =>
+          [...document.querySelectorAll('div.ev2page script')].map(elem => elem.innerText),
+        )
 
-      const shortenedData = data.slice(0, 5)
+        const shortenedData = data.slice(0, 5)
 
-      const convertedData = shortenedData.map(entry => {
-        const parsedData = JSON.parse(entry)
-        const { name: festivalName, description, startDate } = parsedData
-        const { name: locationName } = parsedData.location
-        const { addressLocality: address } = parsedData.location.address
-        const { latitude, longitude } = parsedData.location.geo
-        const { price, priceCurrency } = parsedData.offers
+        const convertedData = shortenedData.map(entry => {
+          const parsedData = JSON.parse(entry)
+          const { name: festivalName, description, startDate } = parsedData
+          const { name: locationName } = parsedData.location
+          const { addressLocality: address } = parsedData.location.address
+          const { latitude, longitude } = parsedData.location.geo
+          const { price, priceCurrency } = parsedData.offers
 
-        // ISO date to timestamp https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
-        const newData = {
-          genre: genre,
-          data: {
-            festivalName,
-            description,
-            startDate: Date.parse(startDate),
-            location: {
-              locationName,
-              address,
-              latitude,
-              longitude,
+          // ISO date to timestamp https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
+          const newData = {
+            genre: genre,
+            data: {
+              festivalName,
+              description,
+              startDate: Date.parse(startDate),
+              location: {
+                locationName,
+                address,
+                latitude,
+                longitude,
+              },
+              price,
+              priceCurrency,
             },
-            price,
-            priceCurrency,
-          },
-        }
+          }
 
-        return newData
-      })
-      // I dond't get this working with the spread Syntax
-      mergedData.push(convertedData)
-      if (i + 1 === musicGenres.length) {
-        console.log(mergedData)
-        console.log(convertedData)
-        console.log(Date.now())
-        // MergedData had to be stored in the database
+          return newData
+        })
+        // I dond't get this working with the spread Syntax
+        mergedData.push(convertedData)
+        if (i + 1 === musicGenres.length) {
+          console.log(mergedData)
+          console.log(convertedData)
+          console.log(Date.now())
+          // MergedData had to be stored in the database
+        }
       }
-    }
-    await browser.close()
-  })
+      await browser.close()
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
