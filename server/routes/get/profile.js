227bcbd61a993710b29b04134bed1220cfb19d6f
Chelsea <chelsea.doeleman@gmail.com>
2020-04-06 13:43:49 +0200

    refactor(profile.js): change function logic for more readable code and using mongoose correctly

--- server/routes/get/profile.js
+++ server/routes/get/profile.js
@@ -4,38 +4,41 @@ import User from '../../database/models/user.js'
 export const profile = async (req, res) => {
   const { id } = req.params
   const { username } = req.query
-  if (req.user) {
-    const avatar = '/assets/images/avatar.svg'
-    await User.findOne({ _id: id, username: username }, async (err, results) => {
-      if (err) return res.redirect('back')
-
-      if (results) {
-        console.log(req.user.matched)
-        const matches = await User.find({ _id: { $in: req.user.matched } }, results => results)
-        const matchName = matches.map(match => {
-          const name = match.username
-          return name
-        })
-        res.render('profile', {
-          data: results,
-          removeButton: matchName.includes(username) ? true : false,
-          navigation: dataNavigation,
-          username: req.user.username,
-          avatar: req.user.avatar || avatar,
-          name: req.user.name || req.user.username,
-        })
-      } else {
-        const notificationMessage = req.flash('error')[0]
-        res.render('profile', {
-          data: results,
-          navigation: dataNavigation,
-          username: req.user.username,
-          name: req.user.name || req.user.username,
-          notificationMessage,
-        })
-      }
-    })
-  } else {
+  if (!req.user) {
     res.redirect('/login')
+  } else {
+    let profile
+    const avatar = '/assets/images/avatar.svg'
+    try {
+      profile = await User.findOne({ _id: id, username: username })
+    } catch (e) {
+      res.redirect('back')
+    }
+
+    if (profile) {
+      const matches = await User.find({ _id: { $in: req.user.matched } })
+      const matchName = matches.map(match => {
+        const name = match.username
+        return name
+      })
+      res.render('profile', {
+        data: profile,
+        removeButton: matchName.includes(username) ? true : false,
+        navigation: dataNavigation,
+        id: req.user.id,
+        username: req.user.username,
+        avatar: req.user.avatar || avatar,
+        name: req.user.name || req.user.username,
+      })
+    } else {
+      const notificationMessage = req.flash('error')[0]
+      res.render('profile', {
+        data: profile,
+        navigation: dataNavigation,
+        username: req.user.username,
+        name: req.user.name || req.user.username,
+        notificationMessage,
+      })
+    }
   }
 }
