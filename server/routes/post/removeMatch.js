d7950248825d3433988298a644897e53a8fc34d5
Chelsea <chelsea.doeleman@gmail.com>
2020-04-10 21:50:09 +0200

    refactor(removeMatch.js): toggle modal before deleting match

--- server/routes/post/removeMatch.js
+++ server/routes/post/removeMatch.js
@@ -2,43 +2,41 @@ import User from '../../database/models/user.js'
 
 export const removeMatch = (req, res) => {
   const { matchID } = req.body
-  User.updateOne(
-    { _id: req.session.passport.user },
-    { $pull: { matched: matchID }, $addToSet: { disliked: matchID } },
-    (err, succes) => {
-      if (err) return res.redirect('back')
-      if (succes) {
-        User.updateOne(
-          { _id: matchID },
-          {
-            $pull: { matched: req.session.passport.user },
-            $addToSet: { disliked: req.session.passport.user },
-          },
-          (err, done) => {
-            if (err) return res.redirect('back')
-            if (done) {
-              User.findOne({ _id: matchID }, (err, person) => {
-                if (req.xhr) {
-                  if (err) res.redirect('back')
-                  if (person) {
-                    const clientData = {
-                      username: person.username,
-                      avatar: person.avatar,
-                      match: false,
-                    }
-                    return res.send(JSON.stringify(clientData))
-                  }
-                } else {
-                  req.flash('matchedavatar', person.avatar)
-                  req.flash('matcheduser', person.username)
-                  //
-                  res.redirect('home')
-                }
-              })
-            }
-          },
-        )
+
+  User.findOne({ _id: matchID }, (err, person) => {
+    if (req.xhr) {
+      if (err) res.redirect('back')
+      if (person) {
+        const clientData = {
+          username: person.username,
+          avatar: person.avatar,
+        }
+        deleteFromMatches()
+        return res.send(JSON.stringify(clientData))
       }
-    },
-  )
+    } else {
+      req.flash('matchedavatar', person.avatar)
+      req.flash('matcheduser', person.username)
+      deleteFromMatches()
+      res.redirect('home')
+    }
+  })
+
+  const deleteFromMatches = async () => {
+    try {
+      await User.updateOne(
+        { _id: req.session.passport.user },
+        { $pull: { matched: matchID }, $addToSet: { disliked: matchID } },
+      )
+      await User.updateOne(
+        { _id: matchID },
+        {
+          $pull: { matched: req.session.passport.user },
+          $addToSet: { disliked: req.session.passport.user },
+        },
+      )
+    } catch (err) {
+      res.redirect('back')
+    }
+  }
 }
