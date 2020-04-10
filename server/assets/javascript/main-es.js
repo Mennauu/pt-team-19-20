7b7c7fa821f9c8ce39b59af77a8901dbd685af2c
Chelsea <chelsea.doeleman@gmail.com>
2020-04-10 13:16:04 +0200

    feat(main-es.js): Add Remove component

--- server/assets/javascript/main-es.js
+++ server/assets/javascript/main-es.js
@@ -2,7 +2,7 @@
 import FormUpload from '@components/form-elements/form-upload/form-upload'
 import FormLogin from '@components/form-login/form-login'
 import FormRegister from '@components/form-registration/form-registration'
-// import Remove from '@components/form-remove/form-remove'
+import Remove from '@components/form-remove/form-remove'
 import FormSettings from '@components/form-settings/form-settings'
 import Match from '@components/match/match'
 // Utilities
@@ -14,4 +14,4 @@ moduleInit.sync('[js-hook-form-register]', FormRegister)
 moduleInit.sync('[js-hook-form-settings]', FormSettings)
 moduleInit.sync('[js-hook-input-file]', FormUpload)
 moduleInit.sync('[js-hook-form-match]', Match)
-// moduleInit.sync('[js-hook-form-remove]', Remove)
+moduleInit.sync('[js-hook-form-remove]', Remove)
