d50190d06c0dc38c6797f83aad34cd4a5909cab9
Chelsea <chelsea.doeleman@gmail.com>
2020-04-14 15:15:02 +0200

    feat(server & routeHandler): add updateProfile to post routes

--- server/server.js
+++ server/server.js
@@ -109,6 +109,7 @@ app.post('/register-user', route.registerUser)
 app.post('/user-settings', upload.single('avatar'), route.userSettings)
 app.post('/user-matches', route.userMatches)
 app.post('/remove-match', route.removeMatch)
+app.post('/update-profile', route.updateProfile)
 app.post(
   '/login-authenticate',
   auth.authenticate('local', {
