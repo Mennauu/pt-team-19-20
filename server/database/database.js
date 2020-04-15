92513530d3174aa58c7f1ffa7437fe261b755091
benl95 <beno_1995@hotmail.com>
2020-04-15 12:51:15 +0200

    Installed required packages for rate-limit

--- server/database/database.js
+++ server/database/database.js
@@ -26,3 +26,32 @@ class Database {
 }
 
 module.exports = new Database()
+
+// Limit requests to max 5 per second
+const { RateLimiterMongo } = require('rate-limiter-flexible')
+require('dotenv').config({ path: '.env' })
+
+const mongoOpts = {
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE, // Won't stop trying to reconnect
+  reconnectInterval: 100, // Reconnect every 100ms
+}
+
+mongoose.connect(uristring, mongoOpts).catch(err => {})
+const mongoConn = mongoose.connection
+
+const opts = {
+  storeClient: mongoConn,
+  points: 5, // Number of allowed requests by user
+  duration: 1, // Max 5 requests per second
+}
+
+const rateLimiterMongo = new RateLimiterMongo(opts)
+rateLimiterMongo
+  .consume(remoteAddress, 1) // Consume 1 point on request
+  .then(rateLimiterRes => {
+    console.log('1 point consumed')
+  })
+  .catch(rateLimiterRes => {
+    res.status(429).send('Too many requests')
+  })
